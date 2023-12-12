4249113c4a4025826b75ff2264df896a48f4e5cd
yurisuika <yurisuika@protonmail.com>
2023-12-12 03:49:54 -0800

    go live

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -1,6 +1,3 @@
-import { RevealEffects } from './js/revealDirect.js';
-import { RevealEffectsMasked } from './js/revealMasked.js';
-
 function addButtonSidebar() {
   var btn = document.createElement("button");
   btn.classList.add("ZehnButton");
@@ -89,7 +86,6 @@ function moveChildElements(target, classes) {
 moveElements(
   '[class*="steamdesktop_TitleBarControls_"]',
   [
-    // '[class*="titlebarcontrols_AccountMenu_"]',
     '[class*="titlebarcontrols_NotificationButtonContainer_"]',
     '[class*="bottombar_FriendsButton_"]',
     '[class*="bottombar_DownloadStatus_"]',
@@ -97,73 +93,5 @@ moveElements(
     '[class*="titlebarcontrols_AnnouncementsButton_"]',
     '[class*="titlebarcontrols_VRToggle_"]',
     '[class*="titlebarcontrols_GamepadUIToggle_"]'
-    // '[class*="titlebarcontrols_WalletBalance_"]',
-    // '[class*="bottombar_Status_"]'
   ]
 )
-
-// moveChildElements(
-//   '[class*="libraryhome_LibraryHome_"]',
-//   [
-//     '[class*="libraryhomeshowcases_AddShowcaseRow_"]'
-//   ]
-// )
-
-// moveElements(
-//   '[class*="steamdesktop_DragArea_"]',
-//   [
-//     '[class*="bottombar_DetailedDownloadProgress_"]'
-//   ]
-// )
-
-// document.querySelector(".rootmenu_RootMenuButton_2UyOB").style.border = "1px solid transparent";
-// document.querySelector(".DialogButton").style.border = "1px solid transparent";
-
-// let FR1 = new RevealEffects("body", {
-//     selector: document.querySelectorAll('.rootmenu_RootMenuButton_2UyOB')[0],
-//     backgroundGradientSize: 150,
-//     borderGradientSize: 80,
-//     borderLightColor: "rgba(255, 255, 255, 0.25)",
-//     backgroundLightColor: "rgba(255, 255, 255, 0.25)"
-// });
-
-// let FR2 = new RevealEffects("body", {
-//     // selector: document.getElementById('.testOne').querySelectorAll('.testTwo')[0],
-//     selector: document.querySelectorAll('.contextmenu_contextMenuItem_pFo3k')[0],
-//     backgroundGradientSize: 150,
-//     borderGradientSize: 80,
-//     borderLightColor: "rgba(255, 255, 255, 0.25)",
-//     backgroundLightColor: "rgba(255, 255, 255, 0.25)"
-// });
-
-// let FR3 = new RevealEffects(".contextmenu_contextMenuContents_2y2tU", {
-//     selector: document.querySelectorAll('.contextmenu_contextMenuItem_pFo3kS')[1],
-//     backgroundGradientSize: 150,
-//     borderGradientSize: 80,
-//     borderLightColor: "rgba(255, 255, 255, 0.75)",
-//     backgroundLightColor: "rgba(255, 255, 255, 0.75)"
-// });
-
-// let FR4 = new RevealEffects("body", {
-//     selector: document.querySelectorAll('.menu_MenuItem_2jXHP')[1],
-//     backgroundGradientSize: 150,
-//     borderGradientSize: 80,
-//     borderLightColor: "rgba(255, 255, 255, 0.75)",
-//     backgroundLightColor: "rgba(255, 255, 255, 0.75)"
-// });
-
-// let FR_ROOT = new RevealEffects("body", {
-//     selector: document.querySelectorAll('.rootmenu_RootMenuButton_2UyOB')[0],
-//     backgroundGradientSize: 150,
-//     borderGradientSize: 80,
-//     borderLightColor: "rgba(255, 255, 255, 0.25)",
-//     backgroundLightColor: "rgba(255, 255, 255, 0.25)"
-// });
-
-// let FR_BUTTON = new RevealEffects("body", {
-//     selector: document.querySelectorAll('.DialogButton')[0],
-//     backgroundGradientSize: 150,
-//     borderGradientSize: 80,
-//     borderLightColor: "rgba(255, 255, 255, 0.25)",
-//     backgroundLightColor: "rgba(255, 255, 255, 0.25)"
-// });
