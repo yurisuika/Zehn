331a2d55df8ed0943c311422fed3170456ef2ae3
yurisuika <yurisuika@protonmail.com>
2024-02-22 17:43:25 -0800

    well windows rolled it back several days that's great

--- notifications.custom.js
+++ notifications.custom.js
@@ -2,6 +2,12 @@ import { RevealEffects } from './js/revealDirect.js';
 import { RevealEffectsMasked } from './js/revealMasked.js';
 import Zehn from './js/Zehn.js';
 
+if (navigator.userAgent.includes("Linux")) {
+  document.documentElement.classList.add("Linux");
+} else if (navigator.userAgent.includes("Windows")) {
+  document.documentElement.classList.add("Windows");
+}
+
 let FR_NOTIFICATION = new RevealEffects("body", {
     selector: document.querySelectorAll('[class*="desktoptoasts_DesktopToastPopup_"]')[0],
     backgroundGradientSize: 150,
