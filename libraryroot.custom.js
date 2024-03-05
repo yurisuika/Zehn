1309aa8b89199ec28f2ca9ed6c1ad3dcc5760a02
yurisuika <yurisuika@protonmail.com>
2024-03-05 09:53:48 -0800

    js game properties icons

--- libraryroot.custom.js
+++ libraryroot.custom.js
@@ -13,39 +13,39 @@ Zehn.waitForElement('[class*="appproperties_AppProperties_"]').then((element) =>
   console.log(cells);
   [].forEach.call(cells, function (el) {
     if(el.innerText.indexOf("Privacy") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAqElEQVQ4jcWT0Q3DIAxEDRvQEbICK2SFrJBZsgIZoStkBVboCqzw+mO3CJGKKh85CSHMnc+2wAFyBf6SugdgBV5A4YuisfWXMCqxABsQm7utuo+teFGnNFBhUu5igWCuf7Rp1QTLmBtCAHI1gwyEhpOB5EVkFpG9MXnq/tBVxwy7iMxC5yGo61SdpzOed865kb57ONUCh/VdzeMYzjwyxE8Vt/+F+xO8AY8A2x0G9QFzAAAAAElFTkSuQmCC')";
+      el.style.backgroundImage = "var(--icon-privacy)";
       console.log(el)
     }
     if(el.innerText.indexOf("Workshop") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAg0lEQVQ4je2TXQ2AMAyErwQDIAFPaMETGpCABTQg4eOlg8L4Ca+EL1l2zS7tZdkEKAFUwMjGFPQIVMErQIX29L7XvmZJg+t4vnFIANCEusENUccEWYOTAVxoAbI0IIv2jElSuVZmducGSJ6Y5HiJr/kbfKsB0D6ZgTbz+ZvueEeX/sICJzSyl2iBYIMAAAAASUVORK5CYII=')";
+      el.style.backgroundImage = "var(--icon-store)";
       console.log(el)
     }
     if(el.innerText.indexOf("DLC") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAgElEQVQ4je2T0Q2AIAxEr8YFnMUVXIEV2H+D80PQWg6MiZ9e0kC46wM+aiQBAGaGKJI0YdQeAJiarpf6AV8DSK6jsPRJ1tp4KDmPbp+Kv7meCxBCyQPEuQbEcFzFq1tACWTelTvf1oAAycJ7BpTg0jk/y0bT2JO/cA6GfopTHO8dqCLgMZAfyWYAAAAASUVORK5CYII=')";
+      el.style.backgroundImage = "var(--icon-shop)";
       console.log(el)
     }
     if(el.innerText.indexOf("Controller") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAABA0lEQVQ4jaWTMU7DQBBF36ZCUAAnoEBCiIYWfApEB+fgEnCIVKkdXyDp4rTQgEJPEwmBKEi6R+GxZaIokGSkX+zMn7+a/bNJZZvobNW9ROAOKIEBkLXyWeTGwWkitUYYA2fAM7ALnAA7UZsDr8A3cApMgAsAVNRMnasPcV6F++BmaiPQVydLyH01X5J/iVoj8KZ2/3F7ja46rQVu1NkazTU+1duklsAecL6mg49A6gDHQG/BsjzsHAdKoL9gbQ84SlY+plZzAXwA71R7UtcPgEPgGhgFX2L+LDBVixVz58Gp+bOkPgH78Q4j4OqP2Qvgkmqpvmrl4QY2DtRfq7xRbP0bfwC7VGWrQHGArgAAAABJRU5ErkJggg==')";
+      el.style.backgroundImage = "var(--icon-controller)";
       console.log(el)
     }
     if(el.innerText.indexOf("Betas") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAkElEQVQ4jd2TQQ6DMAwENxWPax8CX+Md8BS499jr9FCnMpaBVL2xkuVktdmspUSAXA3AygcL0Bvv4fVSIFZgNuFsJnIHTw2q4Bn2L2C0fmiw2M0CJktUR8ONtGvQh7hDkm5TBVAAkorrkd/gFolf8bdB16AhWX9HaUlQdnqzwSEubPBIuHuqTJ5n/Y3TCSdAb0ZzR8TLe04OAAAAAElFTkSuQmCC')";
+      el.style.backgroundImage = "var(--icon-update-dev)";
       console.log(el)
     }
     if(el.innerText.indexOf("Installed Files") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAP0lEQVQ4jWP8////fwYoYGRkZGQgEbDANCIbhMwmBBgpdQHFgGIXkB0GMMtGYhigWzISwwDdMopdwEhCxsMKAM4qQxMCLDGbAAAAAElFTkSuQmCC')";
+      el.style.backgroundImage = "var(--icon-apps-all)";
       console.log(el)
     }
     if(el.innerText.indexOf("Updates") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA5klEQVQ4jaWTMU4DQQxF3yJaehrK7C3ScAsOQHouAMehoU0OgEQHVTYNkIJtWBqSmualiEfMDkFsyJeske0/tmfkj0phN+pc/fQba7VRr0t+7tTqi7pSZ+oky00itlJfg9srUKud+rhjorJJE9yRaqUCvAEtMGYYFsAJcIZ6q7Z/dN5lneoxcA7cDewMYO5Uag0871Ggh/QH/8bRQbcPLDBKBSxsKK6ArnL7CR/A6Z4TtMBDesIX2+UYivs4LwixpFWe53v+iz2p74lX7vkyBDMtxHQZsXUI7oeYSjk3Qc7lvIhcj78B/FuTIUnKDtMAAAAASUVORK5CYII=')";
+      el.style.backgroundImage = "var(--icon-update)";
       console.log(el)
     }
     if(el.innerText.indexOf("Compatibility") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAA2UlEQVQ4jZ2TURGDMBBElzrAAhZiAQtYaCVgIZXQSsBCLKQSqAQsvH700smkEBh2Jh+Zu9u97F2kAkALeCACCzADAejL3D8AE3Usq0SmGneKcwwlQagkz0BvIqN1AdCl4n6nuLPuAvAABovFI+re4q3lehNMXbiLJFfx9bnj+3CR9NoI3pumeUu6SQpAkNRmR5J+PrT2vtRa3JhUbiLA9CdriRMwZvctn7xMeUxGZUQBcIViCVeO8Qq4jKC2mbFUg+/cR7v7SjGkRcreeX6VM6Jzn2llCoe/8wdNO3uLaMgCuQAAAABJRU5ErkJggg==')";
+      el.style.backgroundImage = "var(--icon-switch)";
       console.log(el)
     }
     if(el.innerText.indexOf("General") !== -1) {
-      el.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAbUlEQVQ4jcWT0Q2AIAwFxbgRK7BCV2D/Dc4fiQ3SUjXGl/DRNndpICRg+SxABeQpLJy5J1FwPU5comBRvZhkBIclHjyVRGBTApR21TNYSVpKa+ReQJfBLF+sVu3N1ujaVv4XbLrwXsKapbffeQdjv9KgCXk0WQAAAABJRU5ErkJggg==)";
+      el.style.backgroundImage = "var(--icon-home)";
       console.log(el)
     }
   });
